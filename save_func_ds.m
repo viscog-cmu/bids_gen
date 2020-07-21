71bcf25a60c2698f50593d0fc152b13b7d0b9a70
nblauch <blauch@cmu.edu>
2020-07-21 15:18:13 -0400

    save blockwise ds for lateralization

--- save_func_ds.m
+++ save_func_ds.m
@@ -1,4 +1,4 @@
-function save_func_ds(subs,space,use_tstat,use_mask,overwrite,experiment)
+function save_func_ds(subs,space,use_tstat,use_mask,overwrite,experiment,glm_name,sm_mm_fwhm)
 
 bids_dir = get_bids_dir(experiment);
 
@@ -14,6 +14,12 @@ else
     m_tag = '';
 end
 
+if sm_mm_fwhm > 0
+    sm_tag = sprintf('_sm-%dmm', sm_mm_fwhm);
+else
+    sm_tag = '';
+end
+
 for sub = subs
     try
         decoding_task_count = 0;
@@ -23,14 +29,23 @@ for sub = subs
         end
         cortex_mask = cosmo_fmri_dataset(sprintf('%s/derivatives/freesurfer/sub-%02d/mri/T1w.cortex.nii',...
           bids_dir,sub));
-        fn = sprintf('%s/spm-floc_vol-%s%s%s.mat', ...
-            sub_dir, space, t_tag, m_tag);
+        fn = sprintf('%s/spm-%s_vol-%s%s%s%s.mat', ...
+            sub_dir, glm_name, space, t_tag, m_tag, sm_tag);
         if exist(fn, 'file') && ~overwrite
             continue
         end
-        model_dir = sprintf('%s/derivatives/matlab/spm/sub-%02d/SPM-floc_vol-%s', ...
-            bids_dir,sub,space);
+        model_dir = sprintf('%s/derivatives/matlab/spm/sub-%02d/SPM-%s_vol-%s%s', ...
+            bids_dir,sub,glm_name,space,sm_tag);
         func_ds = cosmo_trim_spm_labels(cosmo_fmri_dataset([model_dir,'/SPM.mat:beta']));
+
+        if contains(glm_name, 'blockwise')
+            fprintf('manually changing the ds targets for lateralization experiment; blockwise glms needs to be generalized')
+            categs = {'Faces', 'Inverted_Faces', 'Words', 'Inverted_Words', 'Objects', 'Letter_Strings'};
+            for targ_i = 1:length(func_ds.sa.targets)
+                func_ds.sa.targets(targ_i) = find(strcmp(categs, strtok(func_ds.sa.labels(targ_i), ' ')));
+            end
+        end
+
         [~, nan_mask] = cosmo_remove_useless_data(func_ds,1,'all');
         mask = cortex_mask.samples & nan_mask;
         mask_ds = func_ds;
